436ab1dfab92c6309c5b74b5ecc44dbb12cc9399
kevin <kevinmerlika98@gmail.com>
2022-05-19 04:38:28 +0100

    Second Commit

---
+++ app/src/test/java/com/example/virginmoney_fragmentversion/Viewmodel/PeopleViewModelTest.kt
@@ -0,0 +1,26 @@
+package com.example.virginmoney_fragmentversion.Viewmodel
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class PeopleViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun getResponsePeople() {
+    }
+
+    @Test
+    fun getResponseRoom() {
+    }
+}
